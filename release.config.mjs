f19c943c8b2eeaa1563fc3babb2e7cdf79fd3d26
Ihor Haiduk <cookkycok@gmail.com>
2025-02-15 22:46:58 +0200

    fix(ci): fix version build

---
+++ release.config.mjs
@@ -0,0 +1,68 @@
+// @ts-check
+
+const preset = 'conventionalcommits'
+
+/**
+ * @type {import('semantic-release').Options}
+ **/
+export default {
+  branches: [
+    'main',
+    {
+      name: 'release',
+      channel: 'next',
+      prerelease: false,
+    },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    'semantic-release-pinst',
+    ['@semantic-release/commit-analyzer', { preset }],
+    ['@semantic-release/release-notes-generator', {
+      writerOpts: {
+        types: [
+          { type: 'feat', section: 'Features' },
+          { type: 'feature', section: 'Features' },
+          { type: 'fix', section: 'Bug Fixes' },
+          { type: 'perf', section: 'Performance Improvements' },
+          { type: 'revert', section: 'Reverts' },
+          { type: 'docs', section: 'Documentation' },
+          { type: 'style', section: 'Styles', hidden: true },
+          { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+          { type: 'refactor', section: 'Code Refactoring' },
+          { type: 'test', section: 'Tests', hidden: true },
+          { type: 'build', section: 'Build System', hidden: true },
+          { type: 'ci', section: 'Continuous Integration', hidden: true },
+        ],
+      },
+    }],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    ['@semantic-release/git', {
+      assets: ['docs', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} :tada: [skip ci]\n\n${nextRelease.notes}',
+    }],
+    '@semantic-release/github',
+  ],
+  releaseRules: [
+    { type: 'docs', release: 'patch' },
+    { type: 'refactor', release: 'patch' },
+  ],
+}
